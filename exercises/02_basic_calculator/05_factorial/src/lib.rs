cb48af199501579a160166b3df488df2960de639
Emil Grehn <emil@grehn.net>
2025-01-27 16:50:16 +0100

    Solve until 03_03

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -10,6 +10,11 @@
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
+fn factorial(num: u32) -> u32 {
+    if num == 0 {return 1}
+    num * factorial(num - 1)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::factorial;
--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -1,3 +1,5 @@
+use std::os::linux::raw::stat;
+
 struct Ticket {
     title: String,
     description: String,
@@ -18,7 +20,12 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title == "" {panic!("Title cannot be empty")}
+        if title.len() > 50 {panic!("Title cannot be longer than 50 bytes")}
+        if description == "" {panic!("Description cannot be empty")}
+        if description.len() > 500 {panic!("Description cannot be longer than 500 bytes")}
+        let allowed_status = ["To-Do", "In Progress", "Done"];
+        if !allowed_status.contains(&status.as_str()) {panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed")}
         Self {
             title,
             description,
